ed38085249ffcaca3340b09c2f6d4bee8730a9bb
JohnAWC <89763362+JohnAWC@users.noreply.github.com>
2021-09-06 08:51:31 +0800

    Update Class-3.js

--- Class-3.js
+++ Class-3.js
@@ -2,10 +2,7 @@
 var names = ['James','Jack','Flora', 'Mikhil'];
 
 for (i=0; i<names.length; i++) {
-    var node = document.createElement("li");                
-    var textnode = document.createTextNode(names[i] + " , " + names[i].length);        
-    node.appendChild(textnode);
-    document.getElementById("answer").appendChild(node);                              
+    document.getElementById("answer").innerHTML+=(names[i] + " , " + names[i].length + "<br>");                              
 }
 
 
